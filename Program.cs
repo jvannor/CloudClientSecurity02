1c28d3780a8186b3ec413547216b6d11e2e95e6a
Jeff Van Nortwick <jeffvan@microsoft.com>
2021-04-09 13:51:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Azure.Core;
+using Azure.Identity;
+
+namespace CloudClientSecurity02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceScope = Environment.GetEnvironmentVariable("APP_SERVICE_SCOPE");
+            var serviceUri = Environment.GetEnvironmentVariable("APP_SERVICE_URI");
+
+            var credential = new ChainedTokenCredential(
+                new ManagedIdentityCredential(),
+                new EnvironmentCredential());
+
+            var token = credential.GetToken(new TokenRequestContext(new string[] { serviceScope }));
+
+            var functionClient = new HttpClient();
+            functionClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Bearer",
+                token.Token);
+
+            var response = functionClient.GetAsync(serviceUri).Result;
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
